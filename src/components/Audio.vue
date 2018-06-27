85512aa52df754426baf2bff028d9d066e0d1ac1
cameron-henkel <33442122+cameron-henkel@users.noreply.github.com>
2018-06-27 10:37:32 -0500

    Audio component tweaks and comments

--- src/components/Audio.vue
+++ src/components/Audio.vue
@@ -2,7 +2,6 @@
   <div id="audio">
     <div v-show="audioFile!==null">
       <div :id="containsWaveform">
-        <p v-if="audioFile==null">Please select an audio file</p>
       </div>
       <div :id="containsSpectrogram" class="spectrogram" v-show="showSpec">
       </div>
@@ -35,12 +34,14 @@
   export default {
     name: 'Audio',
     props: {
+      // Properties inherited from the parent Audio Container
       baseDirectory: String,
       audioFile: null,
-      contains: String,
+      contains: String, // Which type of audio it is, Kasios or Species
     },
     data: function(){
       return{
+        // Default playback values
         playPauseSymbol: 'media-play',
         audioLength: '00:00',
         currentTime: '00:00',
@@ -50,6 +51,7 @@
       }
     },
     computed: {
+      // Assigns unique ids to the divs for waveform and spectrogram
       containsWaveform: function(){
         return this.contains+"Waveform"
       },
@@ -58,33 +60,51 @@
       },
     },
     watch: {
+      // Update player when new audio file is selected
       audioFile() {
+        // Reset default values
         this.showSpec = false;
         this.waveSpecButton = "Show Spectrogram";
         this.currentTime = '00:00';
         this.audioLength = '00:00';
         this.playPauseSymbol = 'media-play'
+
+        // Load in and draw waveform
         this.waveSurferInstance.load(this.baseDirectory + this.audioFile + '.mp3');
+        
+        // Passes Vue instance to callback functions
         var vmWave = this;
+
+        // When audio is playing set current time
         this.waveSurferInstance.on('audioprocess', function () {
           vmWave.currentTime = new Date(Math.round(
             vmWave.waveSurferInstance.getCurrentTime()) * 1000).toISOString().substr(14, 5);
         });
+        
+        // Reset playback icon when audio clip finishes
+        this.waveSurferInstance.on('finish', function () {
+          vmWave.playPauseSymbol = 'media-play'
+        });
+
+        // Spectrogram load and set total duration
         this.waveSurferInstance.on('ready', function () {
           // Update total audio length when file is loaded
           vmWave.audioLength = new Date(Math.round(
             vmWave.waveSurferInstance.getDuration()) * 1000).toISOString().substr(14, 5);
           // Generate spectrogram object when audio is loaded
           var spectrogram = Object.create(WaveSurfer.Spectrogram);
+
           spectrogram.init({
             wavesurfer: vmWave.waveSurferInstance,
             container: "#"+vmWave.containsSpectrogram,
             fftSamples: vmWave.waveSurferHeight*2,
+            // frequenciesDataUrl: "",
           });
         });
       },
     },
     created () {
+      // Setup wavesurfer on startup for audio playback
       this.$nextTick(() => {
         this.waveSurferInstance = WaveSurfer.create({
           container: '#'+this.containsWaveform,
@@ -98,6 +118,7 @@
       })
     },
     methods: {
+      // Toggles between play/pause and sets correct icon
       play () {
         this.waveSurferInstance.playPause()
         if(this.waveSurferInstance.isPlaying()==false){
@@ -107,12 +128,15 @@
           this.playPauseSymbol = 'media-pause'
         }
       },
+      // Goes back 2s in the audio on button click
       skipBack () {
         this.waveSurferInstance.skipBackward()
       },
+      // Goes forward 2s in the audio on button click
       skipForward () {
         this.waveSurferInstance.skipForward()
       },
+      // Toggles whether to show the spectrogram or the waveform
       showSpectrogram () {
         this.showSpec = !this.showSpec
         if (this.showSpec==true){
@@ -136,6 +160,6 @@
     background-color:whitesmoke;
   }
   .spectrogram {
-    margin-top: -128px;
+    margin-top: -128px; 
   }
 </style>
